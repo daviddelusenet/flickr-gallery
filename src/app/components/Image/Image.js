e9fd1b78b344e304cbcd7e156bef3a0f762ac5f0
David de Lusenet <david.delusenet@random.studio>
2019-05-17 15:25:12 +0200

    Add loading functionality to Image component

--- src/app/components/Image/Image.js
+++ src/app/components/Image/Image.js
@@ -2,17 +2,37 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { StyledImage } from './Image.sc';
 
-const Image = ({ alt, src }) => (
-  <StyledImage alt={alt} src={src} />
-);
+export default class Image extends React.PureComponent {
+  static propTypes = {
+    alt: PropTypes.string,
+    src: PropTypes.string.isRequired,
+  };
 
-Image.propTypes = {
-  alt: PropTypes.string,
-  src: PropTypes.string.isRequired,
-};
+  static defaultProps = {
+    alt: '',
+  };
 
-Image.defaultProps = {
-  alt: '',
-};
+  state = {
+    isLoaded: false,
+  };
 
-export default Image;
+  handleLoad = () => {
+    this.setState({
+      isLoaded: true,
+    });
+  };
+
+  render() {
+    const { alt, src } = this.props;
+    const { isLoaded } = this.state;
+
+    return (
+      <StyledImage
+        alt={alt}
+        isVisible={isLoaded}
+        onLoad={this.handleLoad}
+        src={src}
+      />
+    );
+  }
+}
--- src/app/components/Image/Image.sc.js
+++ src/app/components/Image/Image.sc.js
@@ -1,8 +1,14 @@
 /* eslint import/prefer-default-export: 0 */
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const StyledImage = styled.img`
   display: block;
   flex: 0 1 auto;
+  transition: opacity 200ms ease-out;
+  opacity: 0;
   margin: 0 10px 20px;
+
+  ${({ isVisible }) => isVisible && css`
+    opacity: 1;
+  `};
 `;
